21cfb74ca9664db406dd6976f255201300f276dc
Jesse Scott <jesse@puppetlabs.com>
2018-05-21 16:05:59 -0700

    (MAINT) Update Travis CI testing to be more thorough

--- .travis/test_script.sh
+++ .travis/test_script.sh
@@ -1,15 +1,29 @@
-PR_DIR=$PWD
+#!/bin/bash
+set -x # echo commands with vars expanded
+set -e # exit immediately on error
 
-pdk new module pr_new_modules --template-url=$PR_DIR --skip-interview
+TEMPLATE_PR_DIR=$PWD
 
-pdk new module normal_module --skip-interview
-cd normal_module
-pdk convert --template-url=$PR_DIR --skip-interview --force
+pdk new module new_module --template-url="file://$TEMPLATE_PR_DIR" --skip-interview
+pushd new_module
+cp "$TEMPLATE_PR_DIR/.travis/fixtures/new_provider_sync.yml" ./.sync.yml
+pdk new class new_module
+pdk new defined_type testtype
+pdk new provider testprovider
+pdk new task testtask
+pdk validate
+pdk test unit
+popd
+
+pdk new module convert_from_last_release --skip-interview
+pushd convert_from_last_release
+pdk convert --template-url="file://$TEMPLATE_PR_DIR" --skip-interview --force
 cat convert_report.txt
+popd
 
-cd $PR_DIR
 git clone --depth=1 --branch=master https://github.com/puppetlabs/pdk-templates.git ../master-pdk-templates
-pdk new module pr_diff --template-url=$PR_DIR/../master-pdk-templates --skip-interview
-cd pr_diff
-pdk convert --template-url=$PR_DIR --skip-interview --force
+pdk new module convert_from_master --template-url="file://$TEMPLATE_PR_DIR/../master-pdk-templates" --skip-interview
+pushd convert_from_master
+pdk convert --template-url="file://$TEMPLATE_PR_DIR" --skip-interview --force
 cat convert_report.txt
+popd
